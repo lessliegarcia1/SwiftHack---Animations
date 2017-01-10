711cbfcc941cb0c73485afe400d7ef3f8574aab5
Lesslie Garcia <student@lgarcia1.local>
2017-01-10 11:24:00 -0600

    Initial Commit

---
+++ SwiftHack-Animations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftHack-Animations
+//
+//  Created by Lesslie Garcia on 1/10/17.
+//  Copyright © 2017 Lesslie Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
